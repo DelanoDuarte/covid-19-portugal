b5c69c3ba8c77d3eeb29842ab19b624b083075f7
Delano Junior <delano.duarte@gmail.com>
2020-04-12 23:18:59 +0100

    changing the data source for covid ue. Added some resources to get full data and last week data.

--- api/modules/extractor/extractor_blueprint.py
+++ api/modules/extractor/extractor_blueprint.py
@@ -5,6 +5,7 @@ from spiders.dgs_spider import DGSSpider
 
 import threading
 import operator
+import requests
 
 extractor = Blueprint('extractor', __name__)
 
@@ -20,13 +21,32 @@ def daily_data():
         data = json.load(json_file)
         return {"data": sorted(data, key=operator.itemgetter("date"))}
 
-@extractor.route("/update")
-def update_date():
-    t = threading.Thread(target=start_spider)
-    t.start()
-    return {"data":"Update in Progress"}
-    
+@extractor.route("/full_data/count")
+def data_count():
+    data = get_data_and_filtered()
+    count_cases = sum(map(lambda d: int(d["cases"]), data))
+    count_deaths = sum(map(lambda d: int(d["deaths"]), data))
+    result = {
+        "cases": count_cases,
+        "deaths": count_deaths
+    }
+    return {"data" : result}
 
-def start_spider():
-    processor.crawl(DGSSpider)
-    processor.start()
+@extractor.route("/full_data")
+def full_data():
+    json_filted_data = json.dumps(get_data_and_filtered()) 
+    return {"data": json_filted_data}
+
+@extractor.route("/last_week_data")
+def last_week_data():
+    pass
+
+@extractor.route("/last_week_data/count")
+def last_week_data_count():
+    pass
+
+def get_data_and_filtered():
+    data_covid = requests.get("https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
+    content = data_covid.json()
+    filtered_data = [d for d in content["records"] if d['countriesAndTerritories'] == 'Portugal'] 
+    return filtered_data
